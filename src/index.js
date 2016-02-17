1ca9eb5123c27d383e7b81f5644d481350955f48
Dafrok <88609546@qq.com>
2016-02-17 15:07:46 +0800

    Screen

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import './stylus/main.styl'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import MainNav from './jsx/main-nav.jsx'
+import Screen from './jsx/screen.jsx'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 ReactDOM.render(
   <div>
     <MainNav />
+    <Screen />
   </div>,
   document.getElementById('app'))
