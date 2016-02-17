56ed5daac109ebb2234d2699be17a8ffa915ff5e
George Chung <gerhut@gmail.com>
2016-02-17 15:36:20 +0000

    Revert "increase game performance"
    This reverts commit 0db58dc40c8a1f20512aaaeb88c7a06892ca3a33.

--- game-index.js
+++ game-index.js
@@ -15,16 +15,21 @@ const cartridge = new Uint8Array(rom).buffer
 const game = new Game(cartridge)
 const client = redis.createClient(process.env.REDIS_URL)
 
-setInterval(() => {
+game.on('frame', () => {
   const screenValue = JSON.stringify(Array.from(game.screen))
   client.set('screen', screenValue)
   client.lpop('input', (err, value) => {
     if (err) return error(err.message)
-    if (value == null) return
-    const input = JSON.parse(value)
-    game[input.press ? 'press' : 'depress'](value.index, value.button)
+    if (value != null) {
+      const input = JSON.parse(value)
+      if (input.press) {
+        game.press(value.index, value.button)
+      } else {
+        game.depress(value.index, value.button)
+      }
+    }
   })
-}, 50)
+})
 
 game.start()
 
