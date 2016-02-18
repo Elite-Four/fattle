d0d8ba0de434e7572ab32f7441a279f0cc90a7f4
George Chung <gerhut@gmail.com>
2016-02-18 07:14:36 +0000

    Revert "remove debug in global env"
    This reverts commit 5e25909df87e71c3ada0f321e61cac0b0732d1ca.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const EventEmitter = require('events')
 
 const info = require('debug')('fattle:server:info')
 const debug = require('debug')('fattle:server:debug')
+const error = require('debug')('fattle:server:error')
 const IO = require('socket.io')
 
 const encode = require('./encode')
